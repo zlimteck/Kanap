03ee749d23561bae33b11c4fd357d0316f681035
zlimteck <zlimteck@gmail.com>
2022-10-18 23:16:05 +0200

    Add script to retrieve the id in the url and display the product details in the html

---
+++ front/js/product.js
@@ -0,0 +1,19 @@
+// Recupere l'id du produit depuis l'url.
+const url = new URLSearchParams(document.location.search); 
+const id = url.get("_id");
+
+// Recupere les données du produit depuis l'API 
+fetch("http://localhost:3000/api/products/" + id)
+    .then((response) => response.json())
+    .then((data) => {
+      document.querySelector("title").textContent=`${data.name}`;
+      document.querySelector(".item__img").innerHTML += `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
+      document.querySelector("#title").innerHTML += `<h1 id="title">${data.name}</h1>`;
+      document.querySelector("#price").innerHTML += `<span id="price">${data.price}</span>`;
+      document.querySelector("#description").innerHTML += `<p id="description">${data.description}</p>`;
+        // Boucle pour afficher les options de couleurs
+        data.colors.forEach((color) => {
+           document.querySelector("#colors").innerHTML += `<option value="${color}">${color}</option>`;
+        });
+        console.log(data);
+    });
